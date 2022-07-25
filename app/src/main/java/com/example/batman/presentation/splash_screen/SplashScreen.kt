51787cf17a701aca7b440b29bfd2c5fe6d11db57
medise <medise08@gmail.com>
2022-07-25 22:53:46 +0430

    Second commit

--- app/src/main/java/com/example/batman/presentation/splash_screen/SplashScreen.kt
+++ app/src/main/java/com/example/batman/presentation/splash_screen/SplashScreen.kt
@@ -7,10 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
